c8c74f8b75a7512c69999d465b51efe336285f3d
Laura Gödde <laura.goedde@online.de>
2023-12-01 13:34:22 +0100

    Adds partialUpdate for UserEntity
    Co-authored-by: N4ims <79645839+n4ims@users.noreply.github.com>

--- backend/src/main/java/com/flatfusion/backend/controllers/UserRESTController.java
+++ backend/src/main/java/com/flatfusion/backend/controllers/UserRESTController.java
@@ -3,13 +3,42 @@ package com.flatfusion.backend.controllers;
 import com.flatfusion.backend.entities.UserEntity;
 import com.flatfusion.backend.repositories.UserEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/users")
 public class UserRESTController extends RESTController<UserEntity>{
     @Autowired
+    private UserEntityRepository repository;
+
+    @Autowired
     public UserRESTController(UserEntityRepository repository) {
         super(repository);
+    }
+
+    @PatchMapping("/{userId}")
+    public ResponseEntity<UserEntity> partialUpdateUser(@PathVariable UUID userId, @RequestBody Map<String, ?> updates) {
+        Optional<UserEntity> oldEntity = repository.findById(userId);
+
+
+        if (oldEntity.isPresent()) {
+            UserEntity updatedEntity = oldEntity.get();
+            System.out.println("Old user mail: " + updatedEntity.getEmail());
+
+            repository.partialUpdate(userId, updates);
+            Optional<UserEntity> updatedEntityOptional = repository.findById(userId);
+            updatedEntity = updatedEntityOptional.get();
+            System.out.println("New user mail: " + updatedEntity.getEmail());
+
+            return new ResponseEntity<>(updatedEntity, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
--- backend/src/main/java/com/flatfusion/backend/repositories/UserEntityRepository.java
+++ backend/src/main/java/com/flatfusion/backend/repositories/UserEntityRepository.java
@@ -2,8 +2,21 @@ package com.flatfusion.backend.repositories;
 
 import com.flatfusion.backend.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Map;
 import java.util.UUID;
 
 public interface UserEntityRepository extends JpaRepository<UserEntity, UUID> {
+    @Transactional
+    @Modifying
+    @Query("UPDATE UserEntity u SET " +
+            "u.email = CASE WHEN :#{#updates['email']} IS NOT NULL THEN :#{#updates['email']} ELSE u.email END, " +
+            "u.firstName = CASE WHEN :#{#updates['firstName']} IS NOT NULL THEN :#{#updates['firstName']} ELSE u.firstName END, " +
+            "u.lastName = CASE WHEN :#{#updates['lastName']} IS NOT NULL THEN :#{#updates['lastName']} ELSE u.lastName END " +
+            "WHERE u.id = :userId")
+    void partialUpdate(@Param("userId") UUID userId, @Param("updates") Map<String, ?> updates);
 }
