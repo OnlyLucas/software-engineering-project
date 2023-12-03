066da48b3c4e9ad9e51e7683c8101a64f81d0511
II-NO <nikos.orth@iimple.com>
2023-12-03 20:26:24 +0100

    Adds findByGroupId endpoint to GroupGroceryRESTController

--- backend/src/main/java/com/flatfusion/backend/controllers/GroupGroceryRESTController.java
+++ backend/src/main/java/com/flatfusion/backend/controllers/GroupGroceryRESTController.java
@@ -3,15 +3,38 @@ package com.flatfusion.backend.controllers;
 import com.flatfusion.backend.entities.GroupGroceryEntity;
 import com.flatfusion.backend.repositories.GroupGroceryEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/group-groceries")
 public class GroupGroceryRESTController extends RESTController<GroupGroceryEntity>{
     @Autowired
+    GroupGroceryEntityRepository repository;
+
+    @Autowired
     public GroupGroceryRESTController(GroupGroceryEntityRepository repository) {
         super(repository);
+    }
+
+    @GetMapping("/group/{id}")
+    public ResponseEntity<List<GroupGroceryEntity>> getGroupGroceriesByGroupId(@PathVariable UUID id){
+        logger.info("Get GroupGroceries by group id:  " + id);
+        Optional<List<GroupGroceryEntity>> entities = repository.findAllByGroupId(id);
+
+        if(entities.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(entities.get(), HttpStatus.OK);
     }
 
 }
--- backend/src/main/java/com/flatfusion/backend/repositories/GroupGroceryEntityRepository.java
+++ backend/src/main/java/com/flatfusion/backend/repositories/GroupGroceryEntityRepository.java
@@ -3,7 +3,10 @@ package com.flatfusion.backend.repositories;
 import com.flatfusion.backend.entities.GroupGroceryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface GroupGroceryEntityRepository extends JpaRepository<GroupGroceryEntity, UUID> {
+    Optional<List<GroupGroceryEntity>> findAllByGroupId(UUID uuid);
 }
