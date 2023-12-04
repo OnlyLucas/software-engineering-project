73f41ea7a3d0877c5f140480e23d3b8699515cdb
Laura Gödde <laura.goedde@online.de>
2023-12-04 12:21:17 +0100

    Adds Delete Mapping for GroupGroceries

--- backend/src/main/java/com/flatfusion/backend/controllers/GroupGroceryRESTController.java
+++ backend/src/main/java/com/flatfusion/backend/controllers/GroupGroceryRESTController.java
@@ -5,10 +5,7 @@ import com.flatfusion.backend.repositories.GroupGroceryEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -35,6 +32,12 @@ public class GroupGroceryRESTController extends RESTController<GroupGroceryEntit
         }
 
         return new ResponseEntity<>(entities.get(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<GroupGroceryEntity> deleteGroupGrocery(@PathVariable UUID id) {
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
