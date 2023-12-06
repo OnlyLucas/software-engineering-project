01ecb4b20d7dd2d772ee07effdc621655e89091a
Laura Goedde <laura.goedde@sap.com>
2023-12-06 12:38:31 +0100

    Fixes custom queries for GroupGroceryEntityRepository

--- backend/src/main/java/com/flatfusion/backend/repositories/GroupGroceryEntityRepository.java
+++ backend/src/main/java/com/flatfusion/backend/repositories/GroupGroceryEntityRepository.java
@@ -3,17 +3,18 @@ package com.flatfusion.backend.repositories;
 import com.flatfusion.backend.entities.GroupGroceryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface GroupGroceryEntityRepository extends JpaRepository<GroupGroceryEntity, UUID> {
-    @Query("SELECT g FROM GroupGroceryEntity g WHERE g.groupId = :groupId" +
-            " AND g.isCompleted = true ORDER BY g.timestamp DESC")
-    Optional<List<GroupGroceryEntity>> findCompletedByGroupId(UUID uuid);
+    @Query("SELECT g FROM GroupGroceryEntity g WHERE g.group.id = :groupId" +
+            " AND g.isCompleted = true ORDER BY g.createdAt DESC")
+    Optional<List<GroupGroceryEntity>> findCompletedByGroupId(@Param("groupId") UUID groupId);
 
-    @Query("SELECT g FROM GroupGroceryEntity g WHERE g.groupId = :groupId" +
-            " AND g.isCompleted = false ORDER BY g.timestamp DESC")
-    Optional<List<GroupGroceryEntity>> findUncompletedByGroupId(UUID uuid);
+    @Query("SELECT g FROM GroupGroceryEntity g WHERE g.group.id = :groupId" +
+            " AND g.isCompleted = false ORDER BY g.createdAt DESC")
+    Optional<List<GroupGroceryEntity>> findUncompletedByGroupId(@Param("groupId") UUID groupId);
 }
