7625ed383c950d2942954b8670ee21558411c60f
Laura Gödde <laura.goedde@online.de>
2023-12-07 17:24:33 +0100

    Adds getting payments by group id

--- backend/src/main/java/com/flatfusion/backend/repositories/PaymentEntityRepository.java
+++ backend/src/main/java/com/flatfusion/backend/repositories/PaymentEntityRepository.java
@@ -2,8 +2,14 @@ package com.flatfusion.backend.repositories;
 
 import com.flatfusion.backend.entities.PaymentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PaymentEntityRepository extends JpaRepository<PaymentEntity, UUID> {
+    @Query("SELECT p FROM PaymentEntity p WHERE p.group.id = :groupId ORDER BY p.createdAt DESC")
+    Optional<List<PaymentEntity>> findByGroupId(@Param("groupId") UUID groupId);
 }
