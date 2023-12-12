1c039cfbe0e37698bbfd081db7bdd5c0b06a67b7
OnlyLucas <brennerlucas@icloud.com>
2023-12-12 11:29:19 +0100

    #35 loadScreenElements added to AdapterBudgetDetailGet.java and AdapterBudgetDetailOwe.java

--- app/src/main/java/com/example/software_engineering_project/adapter/AdapterBudgetDetailOwe.java
+++ app/src/main/java/com/example/software_engineering_project/adapter/AdapterBudgetDetailOwe.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,6 +20,8 @@ public class AdapterBudgetDetailOwe extends ArrayAdapter<String> {
 
     private ArrayList<String> list;
     private Context context;
+    private ImageView checkExpenseOwe;
+    private TextView amountOfCredit, nameOfCreditor;
 
 
     public AdapterBudgetDetailOwe(Context context, ArrayList<String> items) {
@@ -36,10 +40,19 @@ public class AdapterBudgetDetailOwe extends ArrayAdapter<String> {
 
             LayoutInflater mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
             convertView = mInflater.inflate(R.layout.adapter_budget_detail_owe, null);
+            loadScreenElements(convertView);
 
         }
 
         return convertView;
+
+    }
+
+    private void loadScreenElements(View convertView) {
+
+        amountOfCredit = convertView.findViewById(R.id.amountOfCredit);
+        checkExpenseOwe = convertView.findViewById(R.id.checkExpenseOwe);
+        nameOfCreditor = convertView.findViewById(R.id.nameOfCreditor);
 
     }
 
