f1eb2a620c3c5e768fa5f691b68a43fe8499e282
N4ims <79645839+N4ims@users.noreply.github.com>
2023-12-12 22:55:32 +0100

    Update insert_data.sql

--- architecture/data_schema/insert_data.sql
+++ architecture/data_schema/insert_data.sql
@@ -1,9 +1,10 @@
 -- Users
-INSERT INTO `users` (`id`, `email`, `username`, `password`, `first_name`, `last_name`) VALUES ('00000000-0000-0000-0000-000000000000', 'john.doe@example.com', 'johndoe', 'password1', 'John', 'Doe');
-INSERT INTO `users` (`id`, `email`, `username`, `password`, `first_name`, `last_name`) VALUES ('00000000-0000-0000-0000-000000000001', 'jane.doe@example.com', 'janedoe', 'password2', 'Jane', 'Doe');
-INSERT INTO `users` (`id`, `email`, `username`, `password`, `first_name`, `last_name`) VALUES ('00000000-0000-0000-0000-000000000002', 'mark.doe@example.com', 'markdoe', 'password3', 'Mark', 'Doe');
-INSERT INTO `users` (`id`, `email`, `username`, `password`, `first_name`, `last_name`) VALUES ('00000000-0000-0000-0000-000000000003', 'mike.doe@example.com', 'mikedoe', 'password4', 'Mike', 'Doe');
-INSERT INTO `users` (`id`, `email`, `username`, `password`, `first_name`, `last_name`) VALUES ('00000000-0000-0000-0000-000000000004', 'emma.doe@example.com', 'emmadoe', 'password5', 'Emma', 'Doe');
+INSERT INTO `users` (`id`, `email`, `username`, `password`, `first_name`, `last_name`, `is_active`) VALUES ('00000000-0000-0000-0000-000000000000', 'john.doe@example.com', 'johndoe', 'password1', 'John', 'Doe', 1);
+INSERT INTO `users` (`id`, `email`, `username`, `password`, `first_name`, `last_name`, `is_active`) VALUES ('00000000-0000-0000-0000-000000000001', 'jane.doe@example.com', 'janedoe', 'password2', 'Jane', 'Doe', 1);
+INSERT INTO `users` (`id`, `email`, `username`, `password`, `first_name`, `last_name`, `is_active`) VALUES ('00000000-0000-0000-0000-000000000002', 'mark.doe@example.com', 'markdoe', 'password3', 'Mark', 'Doe', 1);
+INSERT INTO `users` (`id`, `email`, `username`, `password`, `first_name`, `last_name`, `is_active`) VALUES ('00000000-0000-0000-0000-000000000003', 'mike.doe@example.com', 'mikedoe', 'password4', 'Mike', 'Doe', 1);
+INSERT INTO `users` (`id`, `email`, `username`, `password`, `first_name`, `last_name`, `is_active`) VALUES ('00000000-0000-0000-0000-000000000004', 'emma.doe@example.com', 'emmadoe', 'password5', 'Emma', 'Doe', 1);
+INSERT INTO `users` (`id`, `email`, `username`, `password`, `first_name`, `last_name`, `is_active`) VALUES ('00000000-0000-0000-0000-000000000005', 'inactive.doe@example.com', 'inactivedoe', 'password6', 'Inactive', 'Doe', 0);
 
 -- Groups
 INSERT INTO `living_groups` (`id`, `group_name`, `group_description`, `created_by`) VALUES ('00000000-0000-0000-0000-000000000000', 'Group 1', 'Group description 1', '00000000-0000-0000-0000-000000000000');
@@ -17,7 +18,7 @@ INSERT INTO `group_memberships` (`id`, `group_id`, `user_id`) VALUES ('00000000-
 INSERT INTO `group_memberships` (`id`, `group_id`, `user_id`) VALUES ('00000000-0000-0000-0000-000000000004', '00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000004');
 
 -- Cleaning Templates
-INSERT INTO `cleaning_template` (`id`, `group_id`, `task_name`, `template_description`, `start_date`, `end_date`, `interval`, `created_by`) VALUES ('00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000', 'Clean the floor', 'Ensure that the floor is cleaned and mopped', '2023-11-24', '2024-02-24', '7', '00000000-0000-0000-0000-000000000000');
+INSERT INTO `cleaning_template` (`id`, `group_id`, `task_name`, `template_description`, `start_date`, `end_date`, `cleaning_interval`, `created_by`) VALUES ('00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000', 'Clean the floor', 'Ensure that the floor is cleaned and mopped', '2023-11-24', '2024-02-24', '7', '00000000-0000-0000-0000-000000000000');
 
 -- Cleanings
 INSERT INTO `cleanings` (`id`, `group_id`, `user_id`, `cleaning_template`, `date`, `is_completed`, `completed_at`) VALUES ('00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000', '2023-11-24', true, null);
@@ -34,7 +35,7 @@ INSERT INTO `payments` (`id`, `group_id`, `payment_name`, `amount`, `currency_co
 
 -- Payments Changes
 INSERT INTO `payments_changes` (`id`, `group_id`, `payment_id`, `payment_name`, `amount`, `currency_code`, `paid_by`, `changed_by`, `changed_at`) VALUES ('00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000', 'Rent', '999.00', 'EUR', '00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000', '2023-11-14 15:00:00');
-INSERT INTO `payments_changes` (`id`, `group_id`, `payment_id`, `payment_name`, `amount`, `currency_code`, `paid_by`, `changed_by`) VALUES ('00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000', 'Rent', '1000.00', 'EUR', '00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000');
+INSERT INTO `payments_changes` (`id`, `group_id`, `payment_id`, `payment_name`, `amount`, `currency_code`, `paid_by`, `changed_by`) VALUES ('00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000', 'Rent', '1000.00', 'EUR', '00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000', '2023-11-14 16:00:00');
 
 -- Payment Participations
 INSERT INTO `payment_participations` (`id`, `payment_id`, `group_id`, `user_id`, `participation_amount`, `currency_code`, `is_paid`, `paid_at`) VALUES ('00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000', '00000000-0000-0000-0000-000000000000', '250.00', 'EUR', true, CURRENT_TIMESTAMP);
