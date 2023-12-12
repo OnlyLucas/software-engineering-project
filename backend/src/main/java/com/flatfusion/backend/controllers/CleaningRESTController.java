4f0bf8cbd6b4e08d934b36c74b985839258d9be5
Laura Gödde <laura.goedde@online.de>
2023-12-12 09:10:20 +0100

    #30 Adds endpoints for getting uncompleted CleaningTemplates

--- backend/src/main/java/com/flatfusion/backend/controllers/CleaningRESTController.java
+++ backend/src/main/java/com/flatfusion/backend/controllers/CleaningRESTController.java
@@ -3,15 +3,37 @@ package com.flatfusion.backend.controllers;
 import com.flatfusion.backend.entities.CleaningEntity;
 import com.flatfusion.backend.repositories.CleaningEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/cleanings")
 public class CleaningRESTController extends RESTController<CleaningEntity>{
     @Autowired
+    CleaningEntityRepository repository;
+
+    @Autowired
     public CleaningRESTController(CleaningEntityRepository repository) {
         super(repository);
+    }
+
+    @GetMapping("/cleaning-template/{templateId}/uncompleted")
+    public ResponseEntity<List<CleaningEntity>> getUncompletedCleaningsForCleaningTemplate(@PathVariable UUID templateId) {
+        logger.info("Get uncompleted Cleanings by CleaningTemplate id:  " + templateId);
+        Optional<List<CleaningEntity>> uncompletedCleanings = repository.getUncompletedCleaningsForTemplate(templateId);
+        if(uncompletedCleanings.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(uncompletedCleanings.get(), HttpStatus.OK);
     }
 
 }
--- backend/src/main/java/com/flatfusion/backend/repositories/CleaningEntityRepository.java
+++ backend/src/main/java/com/flatfusion/backend/repositories/CleaningEntityRepository.java
@@ -2,8 +2,14 @@ package com.flatfusion.backend.repositories;
 
 import com.flatfusion.backend.entities.CleaningEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CleaningEntityRepository extends JpaRepository<CleaningEntity, UUID> {
+    @Query("SELECT c FROM CleaningEntity c WHERE c.cleaningTemplate.id = :templateId AND c.isCompleted = false ORDER BY c.date ASC")
+    Optional<List<CleaningEntity>> getUncompletedCleaningsForTemplate(@Param("templateId")UUID templateId);
 }
