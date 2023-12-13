a6a7c99dfea0c95b9256b07d41f0c4da1aea05fb
Laura Gödde <laura.goedde@online.de>
2023-12-13 16:29:49 +0100

    #31 Adds endpoints for getPayments and owePayments for BudgetDetails

--- backend/src/main/java/com/flatfusion/backend/controllers/PaymentParticipationRESTController.java
+++ backend/src/main/java/com/flatfusion/backend/controllers/PaymentParticipationRESTController.java
@@ -26,11 +26,10 @@ public class PaymentParticipationRESTController extends RESTController<PaymentPa
         super(repository);
     }
 
-    //TODO Change and add URL in frontend & Renaming methods
     @GetMapping("/group/{groupId}/user/{userId}/get")
-    public ResponseEntity<List<Object[]>> findByGroupIdAndPaymentCreatedByUserId(@PathVariable UUID groupId, @PathVariable UUID userId){
+    public ResponseEntity<List<Object[]>> getGetPaymentsGroupedByUser(@PathVariable UUID groupId, @PathVariable UUID userId){
         logger.info("Get Payments get by group id:  " + groupId + " and user id: " + userId);
-        Optional<List<Object[]>> entities = paymentParticipationEntityRepository.findByGroupIdAndPaymentCreatedByUserId(groupId, userId);
+        Optional<List<Object[]>> entities = paymentParticipationEntityRepository.getGetPaymentsGroupedByUser(groupId, userId);
 
         if(entities.isEmpty()){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -40,9 +39,9 @@ public class PaymentParticipationRESTController extends RESTController<PaymentPa
     }
 
     @GetMapping("/group/{groupId}/user/{userId}/owe")
-    public ResponseEntity<List<Object[]>> findByGroupIdAndParticipation(@PathVariable UUID groupId, @PathVariable UUID userId){
+    public ResponseEntity<List<Object[]>> getOwePaymentsGroupedByUser(@PathVariable UUID groupId, @PathVariable UUID userId){
         logger.info("Get Payments owe by group id:  " + groupId + " and user id: " + userId);
-        Optional<List<Object[]>> entities = paymentParticipationEntityRepository.findByGroupIdAndParticipation(groupId, userId);
+        Optional<List<Object[]>> entities = paymentParticipationEntityRepository.getOwePaymentsGroupedByUser(groupId, userId);
 
         if(entities.isEmpty()){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
--- backend/src/main/java/com/flatfusion/backend/repositories/PaymentParticipationEntityRepository.java
+++ backend/src/main/java/com/flatfusion/backend/repositories/PaymentParticipationEntityRepository.java
@@ -9,11 +9,20 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface PaymentParticipationEntityRepository extends JpaRepository<PaymentParticipationEntity, UUID> {
-    @Query("SELECT p.user, SUM(p.participationAmount) AS totalAmount FROM PaymentParticipationEntity p " +
-            "WHERE p.group.id = :groupId AND p.payment.paidByUser.id = :userId GROUP BY p.user.id")
-    Optional<List<Object[]>> findByGroupIdAndPaymentCreatedByUserId(UUID groupId, UUID userId);
 
+    // get a table with
+    // 1) User in first column (who owes something to the current user)
+    // 2) Accumulated amount this user owes the current user in the second column
+    // parameters are the current groupId and the userId of the current user
+    @Query("SELECT p.user, SUM(p.participationAmount) AS totalAmount FROM PaymentParticipationEntity p " +
+            "WHERE p.group.id = :groupId AND p.payment.paidByUser.id = :userId AND p.user.id != :userId GROUP BY p.user.id")
+    Optional<List<Object[]>> getGetPaymentsGroupedByUser(UUID groupId, UUID userId);
+
+    // get a table with
+    // 1) User in first column (who the current user owes something to)
+    // 2) Accumulated amount the current user owes this user in the second column
+    // parameters are the current groupId and the userId of the current user
     @Query("SELECT p.payment.paidByUser, SUM(p.participationAmount) AS totalAmount FROM PaymentParticipationEntity p " +
             "WHERE p.group.id = :groupId AND p.user.id = :userId AND p.payment.paidByUser.id != :userId GROUP BY p.payment.paidByUser")
-    Optional<List<Object[]>> findByGroupIdAndParticipation(UUID groupId, UUID userId);
+    Optional<List<Object[]>> getOwePaymentsGroupedByUser(UUID groupId, UUID userId);
 }
