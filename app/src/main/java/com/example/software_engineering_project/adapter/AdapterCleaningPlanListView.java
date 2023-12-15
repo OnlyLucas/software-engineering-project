125bccd77a40276b43cb1f8bda8b8041109ba119
Jonas <jonas05-becker@gmx.de>
2023-12-15 08:22:08 +0100

    #41 Java Docs für die restlichen Adapterklassen (2)

--- app/src/main/java/com/example/software_engineering_project/adapter/AdapterCleaningPlanListView.java
+++ app/src/main/java/com/example/software_engineering_project/adapter/AdapterCleaningPlanListView.java
@@ -22,6 +22,14 @@ import com.example.software_engineering_project.viewmodel.CleaningRepository;
 
 import java.util.List;
 
+/**
+ * Custom adapter for displaying a list of cleaning plans in a ListView.
+ *
+ * This adapter extends ArrayAdapter<CleaningTemplate> and is designed to work with the layout 'adapter_cleaning_plan_list_view'.
+ * It provides a customized view for each item in the ListView, including a number, cleaning template name, next cleaning date, and a button to remove the cleaning plan.
+ *
+ * The getView method is overridden to inflate the layout, load screen elements, and set the content based on the CleaningTemplate object at the specified position in the data set. Additionally, it adds a listener for the remove button.
+ */
 public class AdapterCleaningPlanListView extends ArrayAdapter<CleaningTemplate> {
 
     private Context context;
@@ -33,6 +41,12 @@ public class AdapterCleaningPlanListView extends ArrayAdapter<CleaningTemplate> 
     // The ListViewAdapter Constructor
     // @param context: the Context from the MainActivity
     // @param items: The list of items in our Grocery List
+    /**
+     * The constructor for the AdapterCleaningPlanListView.
+     *
+     * @param context The context in which the adapter is being used.
+     * @param items   The List of CleaningTemplate objects to be displayed in the adapter.
+     */
     public AdapterCleaningPlanListView(Context context, List<CleaningTemplate> items) {
 
         super(context, R.layout.adapter_cleaning_plan_list_view, items);
@@ -42,6 +56,17 @@ public class AdapterCleaningPlanListView extends ArrayAdapter<CleaningTemplate> 
     }
 
     // The method we override to provide our own layout for each View (row) in the ListView
+    /**
+     * Get the view that displays the data at the specified position in the data set.
+     *
+     * This method creates or reuses a view to represent an item in the adapter's data set. It inflates the layout
+     * 'adapter_cleaning_plan_list_view' if the provided convertView is null, and then loads screen elements using the loadScreenElements method. It sets the text of the number, cleaning template name, and next cleaning date TextViews, and adds a listener for the remove button.
+     *
+     * @param position    The position of the item within the adapter's data set.
+     * @param convertView The old view to reuse, if possible.
+     * @param parent      The parent that this view will eventually be attached to.
+     * @return The view corresponding to the data at the specified position.
+     */
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
